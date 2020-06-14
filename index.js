b982f018b3e7b57a789caa0681e5f6d7e42559eb
Harish <>
2020-06-14 20:50:59 +0530

    graphql intro

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+let events = []
+app.use(express.json())
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title:String!
+            description:String!
+            price: Float!
+            date: String!
+
+        }
+        input EventInput {
+            title:String!
+            description:String!
+            price: Float!
+            date: String!
+        }
+        type RootQuery {
+            events : [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput) : Event
+        }
+        schema {
+            query:RootQuery
+            mutation:RootMutation
+        }
+        
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = new EventCollection ({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        });
+        return event.save().then((result) =>{
+            return {...result._doc,_id:result._doc._id.toString()} //for mongodb _id obj to string conversion
+        }  
+        ).catch();
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(5000);
